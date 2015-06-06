23e6fd0bc993610f8149630086760ceb64beb93e
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-06 12:28:51 +0530

    modified Books Class and added spec to print books details

--- test/com/twu/biblioteca/BooksTest.java
+++ test/com/twu/biblioteca/BooksTest.java
@@ -5,14 +5,25 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BooksTest {
 
     @Test
     public void shouldDisplayBookList(){
         ArrayList<Book> booksList = new ArrayList<Book>();
-        booksList.add(new Book("Java", "Jones", "21Feb2019"));
-        booksList.add(new Book("CN", "Stalling", "3Oct1990"));
+
+        Book firstBookstub = mock(Book.class);
+        when(firstBookstub.toString())
+                .thenReturn("Java, Jones, 21Feb2019");
+
+        Book secondBookstub = mock(Book.class);
+        when(secondBookstub.toString())
+                .thenReturn("CN, Stalling, 3Oct1990");
+
+        booksList.add(firstBookstub);
+        booksList.add(secondBookstub);
         Books books = new Books(booksList);
         String expectedList = "Java, Jones, 21Feb2019\nCN, Stalling, 3Oct1990\n";
 
