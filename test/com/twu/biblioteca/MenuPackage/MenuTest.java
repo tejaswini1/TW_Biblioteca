2086d03aac78488950417b5a761c937bb4766caa
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-11 23:47:07 +0530

    added spec for menu model clas for invoking specific class method

--- test/com/twu/biblioteca/MenuPackage/MenuTest.java
+++ test/com/twu/biblioteca/MenuPackage/MenuTest.java
@@ -2,6 +2,7 @@ package com.twu.biblioteca.MenuPackage;
 
 import com.twu.biblioteca.BibliotecaView;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.HashMap;
 
@@ -12,7 +13,18 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class MenuTest {
 
+    @Test
+    public void shouldCallAppropriateClassMethod(){
+        HashMap<Integer, MenuActionPerformable> menuItems = new HashMap<Integer, MenuActionPerformable>();
+        ListAllBooks listAllBooks = mock(ListAllBooks.class);
+        menuItems.put(1, listAllBooks);
+        Menu menu = new Menu(menuItems);
 
+
+        menu.compute(1);
+
+        verify(listAllBooks, Mockito.times(1)).execute();
+    }
 
 
 }
