656c4af8ce54ea00d4ac36cafb89c2c27d0a9506
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-16 16:11:24 +0530

    refactored

---
+++ src/com/twu/biblioteca/demo.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+public abstract class demo {
+    ArrayList <Items> availableItems = new ArrayList<Items>();
+    ArrayList <Items> issuedItems = new ArrayList<Items>();
+
+}
